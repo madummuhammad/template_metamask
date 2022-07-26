980fb704b96063aa7483d7b58c4498658aa7a042
Muhammad Ma'dum <muhammad.madum2018@gmail.com>
2022-07-26 15:45:53 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+  AOS.init();
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
