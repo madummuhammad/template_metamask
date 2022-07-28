deda34639c7d4585876d6eca1ae64a8b2808df1d
Muhammad Ma'dum <muhammad.madum2018@gmail.com>
2022-07-28 16:41:40 +0700

    selesai

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,18 @@
         }
     }
 })
+
+  var stick=$(".star-statistic .stick-container");
+  var jumlah=[242,161,50,26,7,242,161,50,26,7];
+  var max=Math.max.apply(null,jumlah);
+
+  for (var i = 0; i < stick.length; i++) {
+    var persen=(jumlah[i]/max*100);
+    $(stick[i]).html(`<div class="row">
+        <div class="col-10 p-0">
+        <div class="stick bg-danger" style="width:`+persen+`%; height: 100%;"></div>
+        </div>
+        <div class="col-2 p-0">`+jumlah[i]+`</div>
+        </div>
+        `);
+}
